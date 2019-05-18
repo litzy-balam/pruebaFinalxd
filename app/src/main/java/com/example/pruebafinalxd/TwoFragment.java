489219db7b2de926b1d53cb20b9e4c3e5def2b9a
DianaGloria <diana.maciel@cbtis72.edu.mx>
2019-05-18 18:23:31 -0500

    Fragmentsxd

--- app/src/main/java/com/example/pruebafinalxd/TwoFragment.java
+++ app/src/main/java/com/example/pruebafinalxd/TwoFragment.java
@@ -2,27 +2,28 @@ package com.example.pruebafinalxd;
 
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toolbar;
 
 
-public class ElementosFragment extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TwoFragment extends Fragment {
 
-    private Toolbar tolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_elementos, container, false);
+        return inflater.inflate(R.layout.fragment_two, container, false);
     }
 
 }
